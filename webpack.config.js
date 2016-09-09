39ed750d1f553251c0e1e69167fb1a5b7fde5de9
Daniel Lim <hbt@markhamboards.com>
2016-09-09 12:02:53 -0400

    edit config
    edit config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var path = require('path');
 var webpack = require('webpack');
@@ -6,17 +6,20 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     devtool: 'eval-source-map',
+
     entry: [
         'webpack-dev-server/client?http://localhost:3000',
         'webpack/hot/only-dev-server',
         'react-hot-loader/patch',
         path.join(__dirname, 'app/index.js')
     ],
+
     output: {
         path: path.join(__dirname, '/dist/'),
         filename: '[name].js',
         publicPath: '/'
     },
+
     plugins: [
         new HtmlWebpackPlugin({
           template: 'app/index.tpl.html',
@@ -30,11 +33,13 @@ module.exports = {
           'process.env.NODE_ENV': JSON.stringify('development')
         })
     ],
+
     eslint: {
         configFile: '.eslintrc',
         failOnWarning: false,
         failOnError: false
     },
+    
     module: {
         preLoaders: [
             {
